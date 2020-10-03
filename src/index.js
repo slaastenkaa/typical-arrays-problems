46f97b5f54884f6d78c745aa4143d879301a0bdb
slaastenkaa <slaastenkaa@gmail.com>
2020-10-03 23:42:07 +0400

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+exports.min = function min(array) {
+    return Math.min.apply(null, array);
 
-exports.min = function min (array) {
-  return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return Math.max.apply(null, array);
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let a = array.reduce((a, b) => a + b);
+    let b = a / array.length;
+    return b;
+
 }
